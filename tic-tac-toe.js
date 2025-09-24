044b775301ac3bd6034f29c7547076e8b5dd9454
Avinash Devineni <devinenia69420@gmail.com>
2025-09-24 08:54:26 -0500

    Implemented tic tac toe AI with minimax algorithm

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -8,6 +8,7 @@ const GameState = Object.freeze({
 });
 
 let activePlayer = Player.X;
+let aiPlayer = Player.X;
 let gameState = GameState.ONGOING;
 
 const ticTacToeSquares = document.getElementsByClassName("tic-tac-toe-square");
@@ -18,8 +19,12 @@ const boardState = [["NONE", "NONE", "NONE"],
 const xImage = document.getElementById("x-image");
 const oImage = document.getElementById("o-image");
 
+if (aiPlayer == Player.X)
+    makeAIMove();
+
 addEventListener("mousedown", e => {
     if (gameState != GameState.ONGOING) return;
+    if (activePlayer == aiPlayer) return;
 
     const clickedElement = document.elementFromPoint(e.clientX, e.clientY);
     const [squareIdx, isValidSquare] = isElementValidSquare(clickedElement);
@@ -32,16 +37,32 @@ addEventListener("mousedown", e => {
     gameState = getGameState();
 
     if (gameState == GameState.LOSS_O || gameState == GameState.LOSS_X)
-        alert((activePlayer == Player.X ? "X" : "O") + " won!");
+        alert("You won!");
     else if (gameState == GameState.DRAW)
         alert("It's a draw");
-    else activePlayer = activePlayer == Player.X ? Player.O : Player.X;
+
+    else {
+        activePlayer = activePlayer == Player.X ? Player.O : Player.X;
+        makeAIMove();
+        
+        gameState = getGameState();
+        if (gameState == GameState.LOSS_O || gameState == GameState.LOSS_X)
+            alert("You lost...");
+        else if (gameState == GameState.DRAW)
+            alert("It's a draw");
+    }
 });
 
+function rowColToSquareElement(row, col) {
+    const rowStr = row == 0 ? "top" : (row == 1 ? "middle" : "bottom");
+    const colStr = col == 0 ? "left" : (col == 1 ? "middle" : "right");
+    return document.getElementById(`${rowStr}-${colStr}`);
+}
+
 function placeXOrOAtSquare(element) {
-    const _image = activePlayer == Player.X ? xImage.cloneNode(true) : oImage.cloneNode(true);
-    _image.style.display = "inline";
-    element.appendChild(_image);
+    const img = activePlayer == Player.X ? xImage.cloneNode(true) : oImage.cloneNode(true);
+    img.style.display = "inline";
+    element.appendChild(img);
 }
 
 function isElementValidSquare(element) {
@@ -116,4 +137,71 @@ function didPlayerWin(player) {
     if (numInDiagonal == 3) return true;
 
     return false;
+}
+
+function makeAIMove() {
+    let [row, column] = findBestAIMove();
+    boardState[row][column] = aiPlayer == Player.X ? "X" : "O";
+
+    setTimeout(() => {
+        placeXOrOAtSquare(rowColToSquareElement(row, column));
+        activePlayer = activePlayer == Player.X ? Player.O : Player.X;
+    }, Math.random() * 500 + 200);
+}
+
+function miniMax() {
+    let state = getGameState();
+    if (state == GameState.DRAW) return 0;
+
+    if (activePlayer === Player.X) {
+        if (state == GameState.LOSS_O) return Infinity;
+        if (state == GameState.LOSS_X) return -Infinity;
+    }
+
+    else {
+        if (state == GameState.LOSS_X) return Infinity;
+        if (state == GameState.LOSS_O) return -Infinity;
+    }
+
+    let bestValue = -Infinity;
+    for (let row = 0; row < 3; row++) {
+        for (let column = 0; column < 3; column++) {
+            if (boardState[row][column] !== "NONE")
+                continue;
+
+            boardState[row][column] = activePlayer == Player.X ? "X" : "O";
+            activePlayer = activePlayer == Player.X ? Player.O : Player.X;
+            bestValue = Math.max(bestValue, -miniMax());
+
+            boardState[row][column] = "NONE";
+            activePlayer = activePlayer == Player.X ? Player.O : Player.X;
+        }
+    }
+
+    return bestValue;
+}
+
+function findBestAIMove() {
+    let bestMove = [-1, -1];
+    let bestValue = -Infinity;
+    for (let row = 0; row < 3; row++) {
+        for (let column = 0; column < 3; column++) {
+            if (boardState[row][column] !== "NONE")
+                continue;
+
+            boardState[row][column] = aiPlayer == Player.X ? "X" : "O";
+            activePlayer = activePlayer == Player.X ? Player.O : Player.X;
+
+            const res = -miniMax();
+            if (res >= bestValue) {
+                bestValue = res;
+                bestMove = [row, column];
+            }
+
+            boardState[row][column] = "NONE";
+            activePlayer = activePlayer == Player.X ? Player.O : Player.X;
+        }
+    }
+
+    return bestMove;
 }
